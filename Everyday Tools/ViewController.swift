8803fe212a55180d5107d983e300ce14b0d520d1
Viktor Ogay <vaogay@gmail.com>
2024-09-04 09:23:32 +0500

    Initial Commit

---
+++ Everyday Tools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Everyday Tools
+//
+//  Created by Viktor Ogay on 04/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
